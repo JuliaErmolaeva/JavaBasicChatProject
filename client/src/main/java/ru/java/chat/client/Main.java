cb2eb4f13cedf253925701f7da429fa21fd349ce
Юлия <juliarmolaeva@mail.ru>
2023-11-12 22:32:45 +0300

    init commit

---
+++ client/src/main/java/ru/java/chat/client/Main.java
@@ -0,0 +1,7 @@
+package ru.java.chat.client;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ server/src/main/java/ru/java/chat/server/Main.java
@@ -0,0 +1,7 @@
+package ru.java.chat.server;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
